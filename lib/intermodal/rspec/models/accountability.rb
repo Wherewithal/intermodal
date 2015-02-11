9fe554765c20c591723573d3a3d09d8b164d4e7d
Ho-Sheng Hsiao <talktohosh@gmail.com>
2015-02-11 14:02:59 -0700

    [SPEC] Added an additional :all test for accountable models

--- lib/intermodal/rspec/models/accountability.rb
+++ lib/intermodal/rspec/models/accountability.rb
@@ -37,7 +37,6 @@ module Intermodal
 
           context 'when concerned with accountability' do
             let(:model) { subject.class }
-
             it { should belong_to :account }
             it { should validate_presence_of :account }
             it { model.should respond_to :by_account_id }
@@ -55,42 +54,52 @@ module Intermodal
             it { model.should respond_to :get }
 
             context ':all' do
-              let(:collection) { model.get(:all) }
-              it 'should find all resources' do
-                collection.should include(subject)
-                collection.size.should eql(1)
+              context 'when unscoped to account' do
+                let(:collection) { model.get(:all) }
+                it 'should find all resources' do
+                  collection.should include(resource)
+                  collection.size.should eql(1)
+                end
+              end
+
+              context 'when scoped to account' do
+                let(:collection) { model.get(:all, account: account) }
+                it 'should find all resources' do
+                  collection.should include(resource)
+                  collection.size.should eql(1)
+                end
               end
             end
 
             context 'by id' do
               it 'should find resource by id' do
-                model.get(subject.id).should eql(subject)
+                model.get(resource.id).should eql(resource)
               end
 
               it 'should return a writeable resource' do
-                model.get(subject.id).should_not be_readonly
+                model.get(resource.id).should_not be_readonly
               end
             end
 
             context 'by account' do
               it 'should find resource scoped to account' do
-                model.get(subject.id, :account => account).should eql(subject)
+                model.get(resource.id, account: account).should eql(resource)
               end
 
               it 'should find resource scoped to account id' do
-                model.get(subject.id, :account_id => account.id).should eql(subject)
+                model.get(resource.id, account_id: account.id).should eql(resource)
               end
 
               it 'should find a writeable resource scoped to account id' do
-                model.get(subject.id, :account_id => account.id).should_not be_readonly
+                model.get(resource.id, account_id: account.id).should_not be_readonly
               end
 
               it 'should not find resource scoped to a different account' do
-                lambda { model.get(subject.id, :account => different_account) }.should raise_error(ActiveRecord::RecordNotFound)
+                expect { model.get(resource.id, account: different_account) }.to raise_error(ActiveRecord::RecordNotFound)
               end
 
               it 'should not find resource scoped to a different account id ' do
-                lambda { model.get(subject.id, :account_id => different_account.id) }.should raise_error(ActiveRecord::RecordNotFound)
+                expect { model.get(resource.id, account_id: different_account.id) }.to raise_error(ActiveRecord::RecordNotFound)
               end
             end
 
