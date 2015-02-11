37ee959f0949e6715963c48952070b79ce5bfb5f
Ho-Sheng Hsiao <talktohosh@gmail.com>
2015-02-11 14:02:39 -0700

    [CONTROLLERS] Factored Authenticatable concern out of Accoutability concern

---
+++ lib/intermodal/concerns/controllers/authenticatable.rb
@@ -0,0 +1,24 @@
+require 'warden'
+
+module Intermodal
+  module Controllers
+    # This concern adds an interface for warden, allowing access to
+    # the warden user as well as exposing a method to ask warden
+    # for authentication.
+    module Authenticatable
+      extend ActiveSupport::Concern
+
+      included do
+        include Intermodal::Let
+
+        let(:account) { env['warden'].user }
+      end
+
+      protected
+
+      def authentication
+        throw(:warden) unless env['warden'].authenticate?
+      end
+    end
+  end
+end
